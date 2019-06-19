22e5386255747410e9b11e9d9193d3786a573ce3
YsHaNgM <ysh788@gmail.com>
2019-06-19 22:06:20 +0100

    request from web.

--- checker.py
+++ checker.py
@@ -1,12 +1,14 @@
 import json
+import urllib.request
 
-file = json.load(open('formula.json'))
+url = "https://formulae.brew.sh/api/formula.json"
+
+
+file = json.load(urllib.request.urlopen(url))
 for x in file:
-	if 'stable' in x['bottle']:
-		if 'mojave' in x['bottle']['stable']['files']:
-			pass
-		else:
-			#print(json.dumps(x, sort_keys=True, indent=4, separators=(',', ':')))
-			print(x['name'])
-
-            
+    if 'stable' in x['bottle']:
+        if 'mojave' in x['bottle']['stable']['files']:
+            pass
+        else:
+            #print(json.dumps(x, sort_keys=True, indent=4, separators=(',', ':')))
+            print(x['name'])
