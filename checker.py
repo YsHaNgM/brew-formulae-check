36eb691d6ff4371d10959c29d70d722378be5ae9
YsHaNgM <ysh788@gmail.com>
2020-02-21 21:49:34 +0000

    auto get default imput system ver

--- checker.py
+++ checker.py
@@ -1,11 +1,20 @@
 import json
 from argparse import ArgumentParser
 import urllib.request
+import os
+
+macos_ver = {"sierra": 16, "high_sierra": 17, "mojave": 18, "catalina": 19}
+
+
+def get_sysname():
+    for name, ver in macos_ver.items():
+        if int(os.uname().release.split('.', maxsplit=1)[0]) == ver:
+            return name
 
 
 parser = ArgumentParser(description="check compatibility")
 parser.add_argument(
-    '--input', '-in', choices=["sierra", "high_sierra", "mojave", "catalina"], default="mojave")
+    '--input', '-in', choices=["sierra", "high_sierra", "mojave", "catalina"], default=get_sysname())
 # parser.add_argument('--reactions', '-r', action="store_true")
 arguments = parser.parse_args()
 
